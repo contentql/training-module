135e0ccd5e1e1d83d41cc348f974d2eff3086ff4
EswarBenarjee <eswar.bane2002.india@gmail.com>
2023-10-11 16:25:34 +0530

    chore: updating unused imports

--- src/sections/_elearning/details/elearning-course-details-question-item.js
+++ src/sections/_elearning/details/elearning-course-details-question-item.js
@@ -1,5 +1,5 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { useState, useCallback } from 'react';
 
 import Typography from '@mui/material/Typography';
 import AccordionDetails from '@mui/material/AccordionDetails';
