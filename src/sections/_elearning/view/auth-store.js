39daf547e505bfaf953bf8820e9c06ea18e91e04
julydeath <manojkarajada.mk@gmail.com>
2023-10-19 18:46:29 +0530

    init : auth using strapi

---
+++ src/sections/_elearning/view/auth-store.js
@@ -0,0 +1,31 @@
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+
+const store = persist(
+  (set) => ({
+    UserData: {
+      authToken: '',
+      userName: '',
+      isLoggedIn: false,
+    },
+    updateUserData: (userData) => {
+      set(() => ({
+        UserData: userData,
+      }));
+    },
+    removeUserData: () => {
+      set(() => ({
+        UserData: {
+          authToken: '',
+          userName: '',
+          isLoggedIn: false,
+        },
+      }));
+    },
+  }),
+  {
+    name: 'user-data',
+  }
+);
+
+export const useStore = create(store);
