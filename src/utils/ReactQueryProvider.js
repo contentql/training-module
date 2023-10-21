074acebc5db90a713f8fcbb5c1a5c7c8a90897bc
julydeath <manojkarajada.mk@gmail.com>
2023-10-21 19:55:28 +0530

    chore : react query provider

--- src/utils/ReactQueryProvider.js
+++ src/utils/ReactQueryProvider.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { QueryClientProvider, QueryClient } from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
 
