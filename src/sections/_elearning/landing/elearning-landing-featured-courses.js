2b6d944b31d963dce10a41218ec595ba462dde01
EswarBenarjee <eswar.bane2002.india@gmail.com>
2023-10-27 11:35:28 +0530

    chore: updating home page according to course page

--- src/sections/_elearning/landing/elearning-landing-featured-courses.js
+++ src/sections/_elearning/landing/elearning-landing-featured-courses.js
@@ -97,7 +97,7 @@ export default function ElearningLandingFeaturedCourses({ courses }) {
                   pb: { xs: 10, md: 15 },
                 }}
               >
-                <ElearningCourseItem course={course.attributes} vertical id={course.id} />
+                <ElearningCourseItem course={course} vertical />
               </Box>
             ))}
           </Carousel>
