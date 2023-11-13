9083ea99d54ad2a18a7484b77a2e7da24c4e85c6
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2023-11-13 14:02:00 +0530

    chore: added the empty state for my learning

--- src/app/account/my-learning/mylearning.js
+++ src/app/account/my-learning/mylearning.js
@@ -9,14 +9,20 @@ import {
 } from 'react-hook-form';
 
 import Box from '@mui/material/Box';
+import { Link } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import LoadingButton from '@mui/lab/LoadingButton';
 
 import { _mock } from 'src/_mock';
+import Image from 'src/components/image';
+import { paths } from 'src/routes/paths';
+import Iconify from 'src/components/iconify';
+import { RouterLink } from 'src/routes/components';
 import { useUserStore } from 'src/states/auth-store';
 import ElearningCourseItem from 'src/sections/_elearning/list/elearning-course-item';
 
@@ -39,18 +45,17 @@ export default function AccountPersonalView() {
       <Typography variant="h5" sx={{ mb: 3 }}>
         My Courses
       </Typography>
-
-      <Grid
-        container
-        spacing={2}
-        sx={{
-          position: 'relative',
-          ml: { md: -2 },
-          mt: 5,
-        }}
-      >
-        {data &&
-          data.map((course) => {
+      {data?.length > 0 ? (
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            position: 'relative',
+            ml: { md: -2 },
+            mt: 5,
+          }}
+        >
+          {data.map((course) => {
             const newCourse = {
               id: course.id,
               attributes: course,
@@ -70,7 +75,42 @@ export default function AccountPersonalView() {
               </Grid>
             );
           })}
-      </Grid>
+        </Grid>
+      ) : (
+        <Stack
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            position: 'relative',
+            ml: { md: -2 },
+            mt: { xs: 12, md: 4 },
+          }}
+        >
+          <Image
+            alt="Empty State My Learning"
+            src="/assets/images/mylearning/empty-state-mylearning.png"
+            sx={{
+              height: 1,
+              width: { xs: 200, md: 360 },
+              objectFit: 'cover',
+              opacity: 0.6,
+            }}
+          />
+
+          <Link component={RouterLink} href={paths.eLearning.courses} sx={{ pt: 2 }}>
+            <Button
+              sx={{ bgcolor: '#FF774B' }}
+              size="large"
+              variant="contained"
+              startIcon={<Iconify icon="carbon:chevron-left" />}
+            >
+              Explore Now
+            </Button>
+          </Link>
+        </Stack>
+      )}
     </>
   );
 }
