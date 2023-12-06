abd3fdec1a88a3a13db794432015058436ae7cf6
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2023-12-06 19:36:40 +0530

    chore: update certificate content

--- src/sections/certificate/certificate.js
+++ src/sections/certificate/certificate.js
@@ -15,17 +15,17 @@ const Certificate = () => (
           <div className="flex flex-col items-start justify-start gap-[14px]">
             <div className="relative leading-[120%] capitalize inline-block w-[511px]">
               <p className="m-0">Certificate</p>
-              <p className="m-0">of Internship</p>
+              <p className="m-0">of Completion</p>
             </div>
             <div className="relative text-mini leading-[135%] font-medium font-montserrat text-dimgray">
-              This internship program certificate is proudly awarded to
+              This course completion certificate is proudly awarded to
             </div>
           </div>
           <div className="flex flex-col items-start justify-start gap-[17px] text-26xl text-darkslateblue font-montserrat">
-            <div className="relative font-semibold">Naveen Chakka</div>
+            <div className="relative font-semibold">Manikanta Potnuru</div>
             <div className="relative text-mini leading-[135%] font-medium text-dimgray inline-block w-[461px]">
-              For his/her outstanding completion of the compulsory internship program at ContentQL
-              from July to December 2022.
+              For his/her outstanding completion of the course at Ryzolve from July to December
+              2023.
             </div>
           </div>
         </div>
@@ -34,7 +34,7 @@ const Certificate = () => (
             <img className="relative w-[72px] h-[73px]" alt="" src="/vector-14.svg" />
             <img className="relative w-[117px] h-px" alt="" src="/vector-10.svg" />
             <div className="flex flex-col items-start justify-start gap-[4px]">
-              <div className="relative leading-[135%] font-semibold">AKHIL</div>
+              <div className="relative leading-[135%] font-semibold">Tunzi</div>
               <div className="relative text-mini leading-[135%] font-medium text-dimgray inline-block w-[122px]">
                 CEO
               </div>
@@ -44,14 +44,14 @@ const Certificate = () => (
             <img className="relative w-[72px] h-[73px]" alt="" src="/vector-14.svg" />
             <img className="relative w-[117px] h-px" alt="" src="/vector-10.svg" />
             <div className="flex flex-col items-start justify-start gap-[4px]">
-              <div className="relative leading-[135%] font-semibold">Naveen</div>
+              <div className="relative leading-[135%] font-semibold">Amar</div>
               <div className="relative text-mini leading-[135%] font-medium text-dimgray inline-block w-[165px]">
                 Programme Manager
               </div>
             </div>
           </div>
           <div className="flex flex-col items-start justify-start gap-[4px]">
-            <div className="relative leading-[135%] font-semibold">04.02.2022</div>
+            <div className="relative leading-[135%] font-semibold">06.12.2023</div>
             <div className="relative text-mini leading-[135%] font-medium text-dimgray">
               Issuing date
             </div>
