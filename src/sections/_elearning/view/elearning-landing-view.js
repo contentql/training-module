975ac44342f5d48e6f8bf4432d1051ab1f59678e
julydeath <manojkarajada.mk@gmail.com>
2024-01-02 09:52:42 +0530

    chore : pop up

--- src/sections/_elearning/view/elearning-landing-view.js
+++ src/sections/_elearning/view/elearning-landing-view.js
@@ -1,6 +1,10 @@
 'use client';
 
+import Link from 'next/link';
 import { useQuery } from 'react-query';
+import { useState, useEffect } from 'react';
+
+import { Box, Modal, Button, Typography } from '@mui/material';
 
 import { _testimonials } from 'src/_mock';
 import { getCoursesData } from 'src/queries/courses';
@@ -9,10 +13,7 @@ import { SplashScreen } from 'src/components/loading-screen';
 import ElearningNewsletter from '../elearning-newsletter';
 import ElearningLandingFaqs from '../landing/elearning-landing-faqs';
 import ElearningLandingHero from '../landing/elearning-landing-hero';
-import ElearningLandingAbout from '../landing/elearning-landing-about';
 import ElearningTestimonial from '../testimonial/elearning-testimonial';
-import ElearningLandingSummary from '../landing/elearning-landing-summary';
-import ElearningLandingIntroduce from '../landing/elearning-landing-introduce';
 // import ElearningLatestPosts from '../../blog/elearning/elearning-latest-posts';
 import ElearningLandingFeaturedCourses from '../landing/elearning-landing-featured-courses';
 
@@ -22,8 +23,60 @@ import ElearningLandingServices from './elearning-landing-services';
 // ----------------------------------------------------------------------
 
 export default function ElearningLandingView() {
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setOpen(true);
+    }, 3000);
+  }, []);
+
   return (
     <>
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 400,
+            height: 200,
+            // bgcolor: '#ffffff',
+            bgcolor: 'white',
+            // border: '2px solid #000',
+            // boxShadow: 24,
+            p: 1,
+            borderRadius: 2,
+          }}
+        >
+          <Button sx={{ left: '80%', color: 'red' }} onClick={() => handleClose()}>
+            close
+          </Button>
+          <Box sx={{ p: 1, color: 'black', textAlign: 'center' }}>
+            <Typography
+              id="modal-modal-title"
+              variant="h6"
+              component="h2"
+              sx={{ color: '#0D5992' }}
+            >
+              New Offer
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Buy now to get 20% discount.
+            </Typography>
+          </Box>
+
+          <Link href="/courses">
+            <Button sx={{ left: '40%', bgcolor: '#FF774C', mt: 1, color: 'black' }}>Buy Now</Button>
+          </Link>
+        </Box>
+      </Modal>
+
       <ElearningLandingHero />
 
       {/* <ElearningLandingAbout /> */}
