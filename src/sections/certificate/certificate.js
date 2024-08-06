c3a0c7b9348baccbeb35b589627fecc64b1dd5d6
julydeath <manojkarajada.mk@gmail.com>
2024-08-07 00:44:39 +0530

    chore : test

--- src/sections/certificate/certificate.js
+++ src/sections/certificate/certificate.js
@@ -11,7 +11,7 @@ const Certificate = ({ certificateData, certificateNames }) => {
 
   console.log('certificateNames', certificateNames);
   return (
-    <div className="relative top-[calc(50%_-_397px)] left-[calc(50%_-_561.5px)] w-[1123px] h-[794px] text-26xl text-black font-montaga items-center text-center mx-auto">
+    <div className="relative top-[calc(50%_-_397px)] left-[calc(50%_-_561.5px)] w-[1123px] h-[794px] text-26xl text-black font-montaga items-center text-center">
       <div className="absolute top-[calc(50%_-_372px)] left-[calc(50%_-_536.5px)] rounded-21xl box-border w-[1073px] h-[744px] border-[2px] border-solid border-lightgray overflow-hidden">
         <img
           className="absolute top-[calc(50%_-_397px)] left-[calc(50%_-_561.5px)] w-[1123px] h-[794px]"
@@ -19,10 +19,10 @@ const Certificate = ({ certificateData, certificateNames }) => {
           src="/mask-group.svg"
         />
         {/* <div className="absolute top-[calc(50%_-_397px)] left-[calc(50%_-_439.5px)] items-center text-center"> */}
-        <div className="flex flex-col text-center items-start justify-start pt-[38px] px-0 pb-0 gap-[75px]">
-          <div className="flex flex-col items-start justify-start gap-[18px]">
+        <div className="flex flex-col text-center items-center justify-center pt-[38px] px-0 pb-0 gap-[75px]">
+          <div className="flex flex-col items-center justify-center gap-[18px]">
             <img className="relative w-44 h-[35px]" alt="" src="/ryzolve-logo1.svg" />
-            <div className="flex flex-col items-start justify-start gap-[14px]">
+            <div className="flex flex-col items-center justify-center gap-[14px]">
               <div className="relative capitalize leading-[120%] inline-block w-[800px]">
                 <p className="m-0">CERTIFICATE OF RECOGNITION</p>
               </div>
@@ -30,8 +30,12 @@ const Certificate = ({ certificateData, certificateNames }) => {
                 This certificate is presented to
               </div>
             </div>
-            <div className="flex flex-col items-start justify-start gap-[17px] text-26xl text-darkslateblue font-montserrat">
+            <div className="flex flex-col items-center justify-center gap-[17px] text-26xl text-darkslateblue font-montserrat">
               <div className="relative font-semibold">{certificateData?.attributes.username}</div>
+              <p>for completing the course</p>
+              <p className="text-26xl text-darkslateblue">
+                {certificateData?.attributes.courseTitle}
+              </p>
               <div className="relative text-base leading-[135%] font-medium text-dimgray inline-block w-[461px]">
                 The holder of this certficate has completed the required sequence of unit under 26
                 TAC 558.259d Part of the initial 24 hr.in.26.259(b) ,{' '}
@@ -39,15 +43,15 @@ const Certificate = ({ certificateData, certificateNames }) => {
               </div>
             </div>
           </div>
-          <div className="flex flex-row items-end justify-start gap-[100px] text-base text-darkslategray font-montserrat">
-            <div className="flex flex-col items-start justify-start gap-[14px]">
+          <div className="flex flex-row items-center justify-center gap-[100px] text-base text-darkslategray font-montserrat">
+            <div className="flex flex-col items-center justify-center gap-[14px]">
               <img
                 className="relative w-[72px] h-[73px]"
                 alt="signature"
                 src={certificateNames[0]?.signature.data.attributes.url}
               />
               <img className="relative w-[117px] h-px" alt="" src="/vector-10.svg" />
-              <div className="flex flex-col items-start justify-start gap-[4px]">
+              <div className="flex flex-col items-center justify-center gap-[4px]">
                 <div className="relative leading-[135%] font-semibold">
                   {certificateNames[0]?.name}
                 </div>
@@ -56,14 +60,14 @@ const Certificate = ({ certificateData, certificateNames }) => {
                 </div>
               </div>
             </div>
-            {/* <div className="flex flex-col items-start justify-start gap-[14px]">
+            {/* <div className="flex flex-col items-start justify-center gap-[14px]">
                 <img
                   className="relative w-[72px] h-[73px]"
                   alt=""
                   src={certificateNames[1]?.signature.data.attributes.url}
                 />
                 <img className="relative w-[117px] h-px" alt="" src="/vector-10.svg" />
-                <div className="flex flex-col items-start justify-start gap-[4px]">
+                <div className="flex flex-col items-start justify-center gap-[4px]">
                   <div className="relative leading-[135%] font-semibold">
                     {certificateNames[1]?.name}
                   </div>
@@ -72,7 +76,7 @@ const Certificate = ({ certificateData, certificateNames }) => {
                   </div>
                 </div>
               </div> */}
-            <div className="flex flex-col items-start justify-start gap-[4px]">
+            <div className="flex flex-col items-center justify-center gap-[4px]">
               <div className="relative leading-[135%] font-semibold">{formattedDate}</div>
               <div className="relative text-mini leading-[135%] font-medium text-dimgray">
                 Issuing date
