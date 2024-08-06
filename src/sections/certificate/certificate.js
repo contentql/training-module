642cc2c039dabb21c3fb96a19c1f95a212853414
julydeath <manojkarajada.mk@gmail.com>
2024-08-07 00:09:47 +0530

    chore : updated certificate

--- src/sections/certificate/certificate.js
+++ src/sections/certificate/certificate.js
@@ -18,14 +18,13 @@ const Certificate = ({ certificateData, certificateNames }) => {
           alt=""
           src="/mask-group.svg"
         />
-        <div className="absolute top-[calc(50%_-_397px)] left-[calc(50%_-_439.5px)] flex flex-row items-center justify-start gap-[83px]">
+        <div className="absolute top-[calc(50%_-_397px)] left-[calc(50%_-_439.5px)] flex flex-row items-center justify-start gap-[83px] text-center">
           <div className="flex flex-col items-start justify-start pt-[38px] px-0 pb-0 gap-[75px]">
             <div className="flex flex-col items-start justify-start gap-[18px]">
               <img className="relative w-44 h-[35px]" alt="" src="/ryzolve-logo1.svg" />
               <div className="flex flex-col items-start justify-start gap-[14px]">
                 <div className="relative leading-[120%] capitalize inline-block w-[511px]">
-                  <p className="m-0">Certificate</p>
-                  <p className="m-0">of Completion</p>
+                  <p className="m-0">Certificate of Completion of Completion</p>
                 </div>
                 <div className="relative text-mini leading-[135%] font-medium font-montserrat text-dimgray">
                   This course completion certificate is proudly awarded to
@@ -34,9 +33,9 @@ const Certificate = ({ certificateData, certificateNames }) => {
               <div className="flex flex-col items-start justify-start gap-[17px] text-26xl text-darkslateblue font-montserrat">
                 <div className="relative font-semibold">{certificateData?.attributes.username}</div>
                 <div className="relative text-mini leading-[135%] font-medium text-dimgray inline-block w-[461px]">
-                  For his/her completion of the course{' '}
-                  <b>{certificateData?.attributes.courseTitle}</b> at Ryzolve from July to December
-                  2023.
+                  The holder of this certficate has completed the required sequence of unit under 26
+                  TAC 558.259d Part of the initial 24 hr.in.26.259(b) ,{' '}
+                  {certificateData?.attributes.courseTitle}. This certficate is granted by Ryzolve
                 </div>
               </div>
             </div>
@@ -81,7 +80,7 @@ const Certificate = ({ certificateData, certificateNames }) => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-center justify-start gap-[60px] text-center text-[25.71px] text-white font-montserrat">
+          {/* <div className="flex flex-col items-center justify-start gap-[60px] text-center text-[25.71px] text-white font-montserrat">
             <div className="rounded-t-none rounded-b-xl bg-coral flex flex-col items-center justify-start pt-[174px] px-[65px] pb-[106px] gap-[167px]">
               <div className="relative leading-[120%] uppercase font-semibold">
                 <p className="m-0">Verified</p>
@@ -90,14 +89,14 @@ const Certificate = ({ certificateData, certificateNames }) => {
               <img className="relative w-[165.6px] h-[123.8px]" alt="" src="/mask-group1.svg" />
             </div>
             <a className="[text-decoration:none] relative w-[227px] h-10 text-sm text-black">
-              {/* <div className="absolute top-[0px] left-[0px] w-[227px] h-10">
+              <div className="absolute top-[0px] left-[0px] w-[227px] h-10">
                 <a className="[text-decoration:none] absolute top-[23px] left-[0px] font-medium text-[inherit]">
                   contentql.com/verify/DGF683DJ
                 </a>
                 <div className="absolute top-[0px] left-[83px] font-semibold">{`Verify at `}</div>
-              </div> */}
+              </div>
             </a>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
