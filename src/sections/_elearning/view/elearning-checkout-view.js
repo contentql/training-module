8c83f4c163a064580a77e5886cd8a0e92cad424b
julydeath <manojkarajada.mk@gmail.com>
2024-08-09 14:50:32 +0530

    chore : updated stripe

--- src/sections/_elearning/view/elearning-checkout-view.js
+++ src/sections/_elearning/view/elearning-checkout-view.js
@@ -41,7 +41,7 @@ import ElearningCheckoutPersonalDetails from '../checkout/elearning-checkout-per
 // ----------------------------------------------------------------------
 
 const stripePromise = loadStripe(
-  'pk_test_51O14wJSGKNDRcuJuUqGzWCeftvJOpycOZUjVgL5BoNzq82clRNztJYpNZw2mdqFtZrkRCCZVbIpSHSqYTIRpJe6t00WaGaXnpK'
+  'pk_live_51IZKCmJZWHNdQWQL8OnhcYNkF2fmAxB3qzL4PmgxxvpJZWDEW6jFXHp8yCyv2QFOhBVQw0Xpjhql774TZde44qfm00JnGm23NB'
 );
 
 // ----------------------------------------------------------------------
